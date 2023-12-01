f772c66f7e471401d81991ca5c18f260ac65b1e3
vnniciusg <90412694+vnniciusg@users.noreply.github.com>
2023-12-01 14:06:03 -0300

    feat: adição do AtividadePersistenceAdapter

--- backend/src/main/java/br/com/projetointegrador/projetointegrador/infra/adapters/output/persistence/ProjetoPersistenceAdapter.java
+++ backend/src/main/java/br/com/projetointegrador/projetointegrador/infra/adapters/output/persistence/ProjetoPersistenceAdapter.java
@@ -3,9 +3,10 @@ package br.com.projetointegrador.projetointegrador.infra.adapters.output.persist
 import br.com.projetointegrador.projetointegrador.application.ports.output.ProjetoOutputPort;
 import br.com.projetointegrador.projetointegrador.domain.model.Projeto;
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.ProjetoEntity;
-import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.mapper.atividade.ProjetoPersistenceMapper;
+import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.mapper.projeto.ProjetoPersistenceMapper;
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.repository.ProjetoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.List;
 
@@ -16,19 +17,35 @@ public class ProjetoPersistenceAdapter implements ProjetoOutputPort {
     private final ProjetoPersistenceMapper projetoPersistenceMapper;
     @Override
     public Projeto save(Projeto projeto) {
-        ProjetoEntity projetoEntity = projetoPersistenceMapper.toProjetoEntity(projeto);
-        projetoEntity = projetoRepository.save(projetoEntity);
-        return projetoPersistenceMapper.toProjeto(projetoEntity);
+        try{
+            ProjetoEntity projetoEntity = projetoPersistenceMapper.toProjetoEntity(projeto);
+            projetoEntity = projetoRepository.save(projetoEntity);
+            return projetoPersistenceMapper.toProjeto(projetoEntity);
+        }catch(DataIntegrityViolationException e){
+            throw new RuntimeException("Erro ao salvar projeto : " + e.getMessage());
+        }catch (Exception e){
+            throw new RuntimeException("Algo inesperado ocorreu ao salvar o projeto " + e.getMessage());
+        }
     }
 
     @Override
     public List<Projeto> list() {
-        return null;
+        try{
+            return projetoPersistenceMapper.toProjetos(projetoRepository.findAll());
+        }catch (Exception e){
+            throw new RuntimeException("Algo inesperado ocorreu ao listar projetos : "  + e.getMessage());
+        }
+
     }
 
     @Override
     public Boolean delete(Long projetoId) {
-        return null;
+        try{
+            return true;
+        }catch(Exception e){
+            throw new RuntimeException("Algo inesperado ocorreu ao deletar projeto " + projetoId + " : "  + e.getMessage());
+        }
+
     }
 
     @Override
