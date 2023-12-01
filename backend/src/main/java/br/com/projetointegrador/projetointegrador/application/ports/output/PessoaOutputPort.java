f9bb900a7b70c7f89fd55909eb78246e5dc6268d
Diogo Franco <112213551+diogomfranco@users.noreply.github.com>
2023-12-01 15:53:37 -0300

    pessoa service

--- backend/src/main/java/br/com/projetointegrador/projetointegrador/application/ports/output/PessoaOutputPort.java
+++ backend/src/main/java/br/com/projetointegrador/projetointegrador/application/ports/output/PessoaOutputPort.java
@@ -2,7 +2,15 @@ package br.com.projetointegrador.projetointegrador.application.ports.output;
 
 import br.com.projetointegrador.projetointegrador.domain.model.Pessoa.Pessoa;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface PessoaOutputPort {
 
     Pessoa criarPessoa(Pessoa pessoa);
+    List<Pessoa> list();
+    Boolean delete(Long pessoa_id);
+
+    Boolean update(Pessoa pessoa);
+
 }
