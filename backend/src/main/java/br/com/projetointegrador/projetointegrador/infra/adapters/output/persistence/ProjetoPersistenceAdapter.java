7cf2025fd2c152cbd9508f184f05c1b32bdae480
Vinnicius Santos <vinnicius109@gmail.com>
2023-12-03 02:13:52 -0300

    hot fix : ajuste no getPrObetivo

--- backend/src/main/java/br/com/projetointegrador/projetointegrador/infra/adapters/output/persistence/ProjetoPersistenceAdapter.java
+++ backend/src/main/java/br/com/projetointegrador/projetointegrador/infra/adapters/output/persistence/ProjetoPersistenceAdapter.java
@@ -1,16 +1,12 @@
 package br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence;
 
 import br.com.projetointegrador.projetointegrador.application.ports.output.ProjetoOutputPort;
-import br.com.projetointegrador.projetointegrador.domain.dto.CriarProjetoRequestDTO;
-import br.com.projetointegrador.projetointegrador.domain.model.Pessoa.Pessoa;
+import br.com.projetointegrador.projetointegrador.application.dto.projeto.request.CriarProjetoRequestDTO;
 import br.com.projetointegrador.projetointegrador.domain.model.Pessoa.TipoPessoa;
 import br.com.projetointegrador.projetointegrador.domain.model.Projeto;
-import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.AtividadeEntity;
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.PessoaEntity;
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.ProjetoEntity;
-import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.administrador.AdministradorCriaAtividadeEntity;
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.administrador.AdministradorCriaProjetoEntity;
-import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.coordenador.CoordenadorCriaAtivadeEntity;
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.coordenador.CoordenadorCriaProjetoEntity;
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.mapper.projeto.ProjetoPersistenceMapper;
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.repository.PessoaRepository;
@@ -36,7 +32,7 @@ public class ProjetoPersistenceAdapter implements ProjetoOutputPort {
         try{
             ProjetoEntity projetoEntity = new ProjetoEntity();
             projetoEntity.setPrNome(requestDTO.getPrNome());
-            projetoEntity.setPrObjetivo(requestDTO.getPrObjetiv());
+            projetoEntity.setPrObjetivo(requestDTO.getPrObjetivo());
             projetoEntity.setPrRecursos(requestDTO.getPrRecursos());
 
             Optional<PessoaEntity> pessoaEntityOptional = pessoaRepository.findById(requestDTO.getPrIdPessoaCadastra());
