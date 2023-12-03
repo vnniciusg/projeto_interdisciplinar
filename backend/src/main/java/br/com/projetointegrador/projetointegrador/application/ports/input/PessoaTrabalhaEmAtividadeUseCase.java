40536d7608fbe9109b98d1b7461739a0f9481ad2
Vinnicius Santos <vinnicius109@gmail.com>
2023-12-03 11:56:04 -0300

    REFACTOR : ALTERAÇÃO DO NOME

--- backend/src/main/java/br/com/projetointegrador/projetointegrador/application/ports/input/PessoaTrabalhaEmAtividadeUseCase.java
+++ backend/src/main/java/br/com/projetointegrador/projetointegrador/application/ports/input/PessoaTrabalhaEmAtividadeUseCase.java
@@ -1,9 +1,11 @@
 package br.com.projetointegrador.projetointegrador.application.ports.input;
 
+import br.com.projetointegrador.projetointegrador.application.dto.pessoaTrabalhaEmAtividade.request.CreatePessoaTrabalhaEmAtividadeDTO;
 import br.com.projetointegrador.projetointegrador.domain.model.PessoaTrabalhaEmAtividade;
 
 public interface PessoaTrabalhaEmAtividadeUseCase {
 
-    PessoaTrabalhaEmAtividade inserirPessoaEmAtividade();
+    PessoaTrabalhaEmAtividade criarPessoaTrabalhaEmAtividade(CreatePessoaTrabalhaEmAtividadeDTO createPessoaTrabalhaEmAtividadeDTO);
+
 
 }
