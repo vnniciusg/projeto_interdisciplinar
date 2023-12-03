42c9ca724bdeeab897b199e1a1420d3036462dee
Vinnicius Santos <vinnicius109@gmail.com>
2023-12-03 11:57:37 -0300

    feat : criação do repository para persistencia de dados de pessoa trabalha em atividade

--- backend/src/main/java/br/com/projetointegrador/projetointegrador/infra/adapters/output/persistence/repository/PessoaTrabalhaEmAtividadeRepository.java
+++ backend/src/main/java/br/com/projetointegrador/projetointegrador/infra/adapters/output/persistence/repository/PessoaTrabalhaEmAtividadeRepository.java
@@ -2,6 +2,8 @@ package br.com.projetointegrador.projetointegrador.infra.adapters.output.persist
 
 import br.com.projetointegrador.projetointegrador.infra.adapters.output.persistence.entity.PessoaTabalhaEmAtividadeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PessoaTrabalhaEmAtividadeRepository extends JpaRepository<PessoaTabalhaEmAtividadeEntity, Long> {
 }
