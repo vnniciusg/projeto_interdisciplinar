66e07a52df43c1f444f7eabd3d770094e0c4b3b5
Vinnicius Santos <vinnicius109@gmail.com>
2023-12-03 11:58:15 -0300

    fix : estava referenciando projeto antes agora está referenciando atividade

--- backend/src/main/java/br/com/projetointegrador/projetointegrador/domain/model/PessoaTrabalhaEmAtividade.java
+++ backend/src/main/java/br/com/projetointegrador/projetointegrador/domain/model/PessoaTrabalhaEmAtividade.java
@@ -1,5 +1,6 @@
 package br.com.projetointegrador.projetointegrador.domain.model;
 
+import br.com.projetointegrador.projetointegrador.domain.model.Atividade.Atividade;
 import br.com.projetointegrador.projetointegrador.domain.model.Pessoa.Pessoa;
 
 import java.util.Date;
@@ -7,7 +8,7 @@ import java.util.Date;
 public class PessoaTrabalhaEmAtividade {
 
     private Long ptaId;
-    private Projeto ptaProjeto;
+    private Atividade ptaAtividade;
     private Pessoa ptaPessoa;
     private Date ptpDataInicio;
     private Date ptpDataFim;
@@ -17,14 +18,13 @@ public class PessoaTrabalhaEmAtividade {
 
     }
 
-    public PessoaTrabalhaEmAtividade(Long ptaId, Projeto ptaProjeto, Pessoa ptaPessoa, Date ptpDataInicio, Date ptpDataFim) {
+    public PessoaTrabalhaEmAtividade(Long ptaId, Atividade ptaAtividade, Pessoa ptaPessoa, Date ptpDataInicio, Date ptpDataFim) {
         this.ptaId = ptaId;
-        this.ptaProjeto = ptaProjeto;
+        this.ptaAtividade = ptaAtividade;
         this.ptaPessoa = ptaPessoa;
         this.ptpDataInicio = ptpDataInicio;
         this.ptpDataFim = ptpDataFim;
     }
-
 
     public Long getPtaId() {
         return ptaId;
@@ -34,12 +34,12 @@ public class PessoaTrabalhaEmAtividade {
         this.ptaId = ptaId;
     }
 
-    public Projeto getPtaProjeto() {
-        return ptaProjeto;
+    public Atividade getPtaAtividade() {
+        return ptaAtividade;
     }
 
-    public void setPtaProjeto(Projeto ptaProjeto) {
-        this.ptaProjeto = ptaProjeto;
+    public void setPtaAtividade(Atividade ptaAtividade) {
+        this.ptaAtividade = ptaAtividade;
     }
 
     public Pessoa getPtaPessoa() {
